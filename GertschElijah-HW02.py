624a1ec818a64283d8129234690327653a31343d
Rock13hound <rock13hound@gmail.com>
2024-10-29 11:02:30 -0600

    Update and rename HW_2 to GertschElijah-HW02.py

---
+++ GertschElijah-HW02.py
@@ -0,0 +1,12 @@
+# Elijah Gertsch
+# UWYO COSC 1010
+# 10/29/2024
+# HW 02
+# Lab Section: 11
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+Morse_dict= {"A":".-", "B":"-...", "C":"-.-.", "D":"-..", "E":".", "F":"..-.","G":"--.", "H":"....", "I":"..", "J":".---", "K":"-.-", "L":".-..", "M":"--", "N":"-.", "O":"---", "P":".--.", "Q":"--.-", "R":".-.", "S":"...", "T":"-", "U":"..-", "V":"...-", "W":".--", "X":"-..-", "Y":"-.--", "Z":"--.."}
+print(Morse_dict)
